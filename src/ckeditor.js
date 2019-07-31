4205d2f1237bc1f61be03a56d80f43f5907c90f0
Ivan Frolkin <ivan.frolkin@mediainstinctgroup.ru>
2019-07-31 13:43:55 +0300

    alignment and media

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -11,6 +11,7 @@ import UploadAdapter from '@samhammer/ckeditor5-simple-image-upload-plugin';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Alignment from '@ckeditor/ckeditor5-alignment/src/alignment';
 import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
 import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
 import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
@@ -37,6 +38,7 @@ ClassicEditor.builtinPlugins = [
 	Autoformat,
 	Bold,
 	Italic,
+	Alignment,
 	BlockQuote,
 	CKFinder,
 	EasyImage,
@@ -55,6 +57,9 @@ ClassicEditor.builtinPlugins = [
 	TableToolbar
 ];
 
+const IFRAME_SRC = '//cdn.iframe.ly/api/iframe';
+const API_KEY = '91f98e48a48ef9c21af604';
+
 // Editor configuration.
 ClassicEditor.defaultConfig = {
 	toolbar: {
@@ -64,6 +69,7 @@ ClassicEditor.defaultConfig = {
 			'bold',
 			'italic',
 			'link',
+			'alignment',
 			'bulletedList',
 			'numberedList',
 			'imageUpload',
@@ -109,6 +115,36 @@ ClassicEditor.defaultConfig = {
 			} );
 		}
 	},
+	mediaEmbed: {
+		previewsInData: false,
+
+		extraProviders: [
+			{
+				// hint: this is just for previews. Get actual HTML codes by making API calls from your CMS
+				name: 'iframely previews',
+
+				// Match all URLs or just the ones you need:
+				url: /.+/,
+
+				html: match => {
+					const url = match[ 0 ];
+
+					const iframeUrl = IFRAME_SRC + '?app=1&api_key=' + API_KEY + '&url=' + encodeURIComponent(url);
+					// alternatively, use &key= instead of &api_key with the MD5 hash of your api_key
+					// more about it: https://iframely.com/docs/allow-origins
+
+					return (
+						'<div class="iframely-embed" style="max-width: 200px">' +
+							'<div class="iframely-responsive">' +
+								`<iframe src="${ iframeUrl }" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen>` +
+								'</iframe>' +
+							'</div>' +
+						'</div>'
+					);
+				}
+			}
+		]
+	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en'
+	language: 'ru'
 };
